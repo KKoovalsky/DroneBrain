5cb856096d4fae1f187a675bf57f77b5cf365dcf
KKoovalsky <kacper.j.kowalski@student.put.poznan.pl>
2016-08-24 19:08:42 +0200

    Getting data from IMU with wiringPi commands implemented

---
+++ I2C.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DroneBrain
+{
+    public class XyzUshortS
+    {
+
+        public ushort X, Y, Z;
+        public byte[] RcvBuf;
+
+        public XyzUshortS()
+        {
+            RcvBuf = new byte[6];
+        }
+
+        public void assign_data()
+        {
+            X = (ushort)(((ushort)(RcvBuf[0] << 8)) | (ushort)RcvBuf[1]);
+            Y = (ushort)(((ushort)(RcvBuf[2] << 8)) | (ushort)RcvBuf[3]);
+            Z = (ushort)(((ushort)(RcvBuf[4] << 8)) | (ushort)RcvBuf[5]);
+        }
+
+        public override string ToString()   
+        {
+            return X + " " + Y + " " + Z;
+        }
+    }
+
+    internal class I2C
+    {
+        
+        public static void get_XYZ_data(byte devAddr, byte regAddr, ref XyzUshortS dest)
+        {
+            Program.RaspProc.StartInfo.FileName = "i2cget";
+            for (byte i = 0; i < 6; i++)
+            {
+                Program.RaspProc.StartInfo.Arguments = "-y 1 " + devAddr + " " + regAddr;\
+                Program.RaspProc.Start();
+                var readLine = Program.RaspProc.StandardOutput.ReadLine();
+                if (readLine != null)
+                    dest.RcvBuf[i] = Convert.ToByte(readLine.Substring(0, 4), 16);
+                dest.assign_data();
+                regAddr++;
+            }
+
+            Program.check_procbuff_empty();
+        }
+
+        public static void set_byte(byte devAddr, byte regAddr, byte data)
+        {
+            Program.RaspProc.StartInfo.FileName = "i2cset";
+            Program.RaspProc.StartInfo.Arguments = "-y 1 " + devAddr + " " + regAddr + " " + data;
+            Program.RaspProc.Start();
+
+            Program.check_procbuff_empty();
+        }
+    }
+
+    namespace I2C_Regs_Addrs
+    {
+        static class MPU9255 {
+            public const byte DevAddr = 0x68;
+
+            public const byte AccAddr = 59;
+            public const byte GyroAddr = 67;
+
+
+            public const byte IntBypEnAddr = 0x37;
+            public const byte BypEnBit = 0x02;
+
+        }
+
+        static class AK8963
+        {
+            public const byte DevAddr = 0x0C;
+            
+
+            public const byte Cntl1Addr = 0x0A;
+
+            public const byte BitSet = 0x10;
+            public const byte CntnsMode2Set = 0x06;
+
+
+            public const byte MagnAddr = 3;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace DroneBrain
+{
+    class Program
+    {
+        public static Process RaspProc = new Process();
+
+        public static void check_procbuff_empty()
+        {
+            if(!RaspProc.StandardOutput.EndOfStream)
+            {
+                Console.WriteLine("Process buffer not empty. Content:");
+                while(!RaspProc.StandardOutput.EndOfStream) Console.WriteLine(RaspProc.StandardOutput.ReadLine());
+                Console.WriteLine("---------------------------");
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            RaspProc.StartInfo.UseShellExecute = false;
+            RaspProc.StartInfo.RedirectStandardOutput = true;
+
+            var accelerometerData = new XyzUshortS();
+            var magnetometerData = new XyzUshortS();
+            var gyroscopeData = new XyzUshortS();
+
+
+            // Enable bypass mode to get data from magnetometer (AK8963).
+            I2C.set_byte(I2C_Regs_Addrs.MPU9255.DevAddr, I2C_Regs_Addrs.MPU9255.IntBypEnAddr, 
+                I2C_Regs_Addrs.MPU9255.BypEnBit);
+
+            // Set continuous mode of magnetometer data gathering.
+            I2C.set_byte(I2C_Regs_Addrs.AK8963.DevAddr, I2C_Regs_Addrs.AK8963.Cntl1Addr,
+                I2C_Regs_Addrs.AK8963.CntnsMode2Set);
+           
+            Console.WriteLine("Setting up connection with 10DOF IMU...");
+
+            I2C.get_XYZ_data(I2C_Regs_Addrs.MPU9255.DevAddr, I2C_Regs_Addrs.MPU9255.AccAddr, 
+                ref accelerometerData);
+
+            I2C.get_XYZ_data(I2C_Regs_Addrs.MPU9255.DevAddr, I2C_Regs_Addrs.MPU9255.GyroAddr, 
+                ref gyroscopeData);
+
+            I2C.get_XYZ_data(I2C_Regs_Addrs.AK8963.DevAddr, I2C_Regs_Addrs.AK8963.MagnAddr,
+                ref magnetometerData);
+
+            Console.WriteLine(accelerometerData.ToString());
+            Console.WriteLine(gyroscopeData.ToString());
+            Console.WriteLine(magnetometerData.ToString());
+
+           
+        }
+    }
+}
